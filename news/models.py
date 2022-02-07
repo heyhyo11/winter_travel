0d6ddf9ff2ef5a4ef73bb36b094d17727bbf38c2
hi1d <aopd408@gmail.com>
2022-02-07 10:10:00 +0900

    [API]data_crawling

--- news/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Img(models.Model):
-    class Meta:
-        db_table ='img'
-    Img = models.FileField(upload_to='uploads/%Y%m%d')
-    Img_url = models.CharField(max_length=250)
--- project_main/settings.py
+++ project_main/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_page',
     'storages',
-    'news',
+    'API',
 ]
 
 MIDDLEWARE = [
