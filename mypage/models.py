546ed7d61e365843a24e1eae8473dd03c7925e3c
Ho Kim <hopao159@gmail.com>
2022-02-08 16:31:12 +0900

    추가 - db모델 추가

--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import UserModel
 
-# Create your models here.
+class db_recommend(models.Model):
+    class Meta:
+        db_table = 'user_view'
+    category = models.TextField(max_length=500)
+    category_count = models.TextField(max_length=500)
+    user_view = models.TextField(max_length=5000)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, unique=True)
+    last_view_category = models.CharField(max_length=50)
