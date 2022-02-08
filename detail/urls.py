4c66df133f784ed341c9615a5f9ed22ddea0e67b
Ho Kim <hopao159@gmail.com>
2022-02-08 23:07:51 +0900

    수정 - 함수비활성화

--- detail/urls.py
+++ detail/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('detail/<int:id>', views.detail, views.recommand, name='detail'),
+    path('detail/<int:id>', views.detail, name='detail'),
     path('map/', views.thismap, name='maps'),
     path('test/', views.pictures, name='pictures'),
     path('d_p/<int:id>', views.detail_view, name='view'),
