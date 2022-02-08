2e2cf54df9ab708ffd853b731ca73c055df90be0
geang <hsegeang@gmail.com>
2022-02-08 22:52:37 +0900

    git pull 받기전 add

--- project_main/settings.py
+++ project_main/settings.py
@@ -41,11 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_page',
     'storages',
-<<<<<<< HEAD
     # 'news',
     'detail',
-=======
-
     # 유저
     'user',
     # 소셜 로그인
@@ -56,7 +53,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.kakao',
     'allauth.socialaccount.providers.github',
->>>>>>> 141e725052c59b00e85124fb754f33d8c65a8c45
 ]
 
 
@@ -169,12 +165,10 @@ with open(os.path.join(BASE_DIR, 'project_main/config/aws.json')) as f:
 AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
-<<<<<<< HEAD
-AWS_DEFAULT_ACL = 'public-read' 
+ 
 
 AWS_S3_REGION_NAME = "ap-northeast-2"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
-=======
 AWS_DEFAULT_ACL = 'public-read'
 
 
@@ -225,4 +219,3 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
->>>>>>> 141e725052c59b00e85124fb754f33d8c65a8c45
