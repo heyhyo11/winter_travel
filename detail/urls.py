cd54c49ad0515b2eda6bc4be22083a30b0adc969
geang <hsegeang@gmail.com>
2022-02-08 23:23:19 +0900

    git pull 전 add

--- detail/urls.py
+++ detail/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('detail/<int:id>', views.detail, name='detail'),
-    path('map/', views.thismap, name='maps'),
-    path('test/', views.pictures, name='pictures'),
-    path('d_p/<int:id>', views.detail_view, name='view'),
 ]
